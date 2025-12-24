ede104033760b2b12e3c05b3f537be231f312eb7
Syun Naitou <s24c1088gr@s.chibakoudai.jp>
2025-12-24 17:30:19 +0900

    update kadai

--- test.bash
+++ test.bash
@@ -8,29 +8,28 @@ ng () {
 }
 
 res=0
-COMMAND="./loan_money"
 
 ### NOMAL INPUT ###
-out=$(echo "2000 30 2.0" | ./loan_monry)
+out=$(echo "2000 30 2.0" | ./loan_money)
 ["${out}" = "月々の返済額：73,924円" ] || ng"$LINENO"
 
 ## STRANGE INOUT ###
-out=$(echo "a" | ./loan_monry)
+out=$(echo "a" | ./loan_money)
 [ "$?" = 1 ] || ng"$LINENO" 
 ["${out}" = "" ] || ng"$LINENO"
 
-out=$(echo "a b" | ./loan_monry)
+out=$(echo "a b" | ./loan_money)
 [ "$?" = 1 ] || ng"$LINENO"
 ["${out}" = "" ] || ng"$LINENO"
 
-out=$(echo "a b c" | ./loan_monry)
+out=$(echo "a b c" | ./loan_money)
 [ "$?" = 1 ] || ng"$LINENO"
 ["${out}" = "" ] || ng"$LINENO"
 
-out=$(echo "10 2" | ./loan_monry)
+out=$(echo "10 2" | ./loan_money)
 [ "$?" = 1 ] || ng"$LINENO"
 ["${out}" = "" ] || ng"$LINENO"
 
-out=$(echo "10 2 3.0 4" | ./loan_monry)
+out=$(echo "10 2 3.0 4" | ./loan_money)
 [ "$?" = 1 ] || ng"$LINENO"
 ["${out}" = "" ] || ng"$LINENO"
