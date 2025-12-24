70b9538da51a44e7fc2fa409b956a547243f592f
Syun Naitou <s24c1088gr@s.chibakoudai.jp>
2025-12-24 17:22:00 +0900

    update

--- test.bash
+++ test.bash
@@ -1,31 +1,36 @@
-#!/usr/bin/bash -xv
-#SPDX-FileCopyrightText: 2025 Syun Naitou
-#SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Syun Naitou
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違う
+        echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
+COMMAND="./loan_money"
 
-#nomal command#
-a=$(seq 5 | ./plus)
+### NOMAL INPUT ###
+out=$(echo "2000 30 2.0" | ./loan_monry)
+["${out}" = "月々の返済額：73,924円" ] || ng"$LINENO"
 
-[ "$a" = 15 ] || ng "$LINENO"
+## STRANGE INOUT ###
+out=$(echo "a" | ./loan_monry)
+[ "$?" = 1 ] || ng"$LINENO" 
+["${out}" = "" ] || ng"$LINENO"
 
-#stlenge command#
+out=$(echo "a b" | ./loan_monry)
+[ "$?" = 1 ] || ng"$LINENO"
+["${out}" = "" ] || ng"$LINENO"
 
-a=$(echo あ | ./plus)
+out=$(echo "a b c" | ./loan_monry)
+[ "$?" = 1 ] || ng"$LINENO"
+["${out}" = "" ] || ng"$LINENO"
 
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${a}" = "" ] || ng "$LINENO"
+out=$(echo "10 2" | ./loan_monry)
+[ "$?" = 1 ] || ng"$LINENO"
+["${out}" = "" ] || ng"$LINENO"
 
-a=$(echo | ./plus)
-
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${a}" = "" ] || ng "$LINENO"
-
-["$res" = 0 ] && echo OK
-
-exit $res
+out=$(echo "10 2 3.0 4" | ./loan_monry)
+[ "$?" = 1 ] || ng"$LINENO"
+["${out}" = "" ] || ng"$LINENO"
